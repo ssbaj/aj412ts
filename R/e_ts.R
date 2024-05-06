6dcc0ea7bf6e22f1d3b5a55b3d880620d154e64d
ssbaj <ssb9018@ajou.ac.kr>
2024-05-06 13:17:21 +0900

    first commit

--- R/e_ts.R
+++ R/e_ts.R
@@ -3,9 +3,19 @@
 e_ts<-function(explaining=0){
 if(explaining==0) {
 cat("    ", '\n')
-cat("# create Time Series Objects  ", '\n')
+cat("# create Time Series Objects ------------- ", '\n')
 cat("  gold <- ts(log(dat$gold),start=c(1993,11),frequency=12)  ", '\n')
-cat("  silver <- ts(log(dat$silver),start=c(1993,11),frequency=12)  ", '\n')
-cat("  plat <- ts(log(dat$plat),start=c(1993,11),frequency=12)  ", '\n')
-cat("  pall <- ts(log(dat$pall),start=c(1993,11),frequency=12)  ", '\n')
+cat("    ", '\n')
+cat("# xts의 apply.quarterly() 사용 ------------- ", '\n')
+cat(" \033[1;32m 자료1<-xts(전산업생산지수$ind_m, order.by=전산업생산지수$date)   \033[0m ", '\n')
+cat(" \033[1;32m 자료2<-apply.quarterly(자료1, FUN=colMeans)  \033[0m ", '\n')
+cat("    ", '\n')
+cat("# xts를 ts로 전환 ------------- ", '\n')
+cat("  library(dsa)  ", '\n')
+cat("  tmp <- xts2ts(xtsdata)  ", '\n')
+cat("    ", '\n')
+cat("# 경제성장률(분기별 gdp) ------------- ", '\n')
+cat(" \033[1;32m 전년동기대비_GDP성장률 <- (gdp / stats::lag(gdp, 4) -1 )*100    \033[0m ", '\n')
+cat("    ", '\n')
+
 }}
