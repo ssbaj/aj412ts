bd149ea537bfaf2fa85b9b7107892c3af3985b81
ssbaj <ssb9018@ajou.ac.kr>
2024-11-02 15:43:56 +0900

    first commit

--- R/simple_stock.R
+++ R/simple_stock.R
@@ -1,6 +1,15 @@
 simple_stock<-function(stock_data){
+
+if (base::missing(stock_data)) {
+	  cat("  DATE는 as.Date로 자동 지정됨 ", '\n')
+      cat("  NA미싱데이터는 자동을 삭제됨 ", '\n')
+	  cat("  xtsdf <- xts(df$stockprice, order.by=df$DATE)  ", '\n')
+	  return( cat("       plot(xtsdata, col = 'blue') ", '\n') )
+	}
+
 df<-stock_data
 df<-as.data.frame(df)
+df<-df[ complete.cases(df), ]
 df$DATE<-rownames(df)
 df$DATE<-as.Date(df$DATE)
 df<-df[,c(7, 4)]
