c46a68a7b8d8b116007db4acfc3ccabc6a67403d
ssbaj <ssb9018@ajou.ac.kr>
2025-01-25 15:32:13 +0900

    first commit

--- R/uroot.R
+++ R/uroot.R
@@ -1,4 +1,9 @@
-uroot <- function(x, nlag=4, method=c("adf"), lag.short=TRUE) {
+uroot <- function(x, nlag=4, method=c("adf"), lag.short=TRUE, out=TRUE) {
+
+if (base::missing(x)) {
+            cat(' # How to use --------------------------   ', '\n')
+	    return(cat("  uroot(df$cs, nlag=4, method=c('adf', 'pp', 'kpss'), lag.short=TRUE, out=TRUE) "))  }
+
 
 if (!require(aTSA)) {
     cat('Installing aTSA package for uroot .....', '\n')
@@ -7,8 +12,6 @@ if (!require(aTSA)) {
   
 suppressPackageStartupMessages(library("aTSA"))
 
-RE_uroot <- stationary.test(x, method=method, nlag=nlag, lag.short=lag.short)
-
-return(RE_uroot)
+return(stationary.test(x, method=method, nlag=nlag, lag.short=lag.short, out=out))
 }
 
