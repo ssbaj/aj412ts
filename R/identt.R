3f2801935d8c9d864ee16f0c4e52f0260fd3a179
ssbaj <ssb9018@ajou.ac.kr>
2026-01-07 17:14:01 +0900

    first commit

--- R/identt.R
+++ R/identt.R
@@ -1,26 +1,35 @@
-# identt() Function
-#
-# This function draws ACF & PACF
-# 
+#'
+#' identt() Function
+#'
+#' This function draws vertical ACF & PACF as barplots
+#'
 
-identt<-function(xdatas, lagxx=15){
+identt <- function(xdatas, lagx2 = 15){
 
-if (base::missing(xdatas)) {
-	 return(cat("  identt(df, lag:You decide or blank:automatic_allocation ) "))}
+  if (base::missing(xdatas)) {
+    return(cat("  identt(df, lag: You decide or blank: automatic_allocation ) "))
+  }
 
+  tm_length <- length(xdatas)
 
- tm_length<-length(xdatas)
- if(tm_length<=40) {lagx<-round(tm_length*(2/3), 0)}
- else if(tm_length<=60) {lagx<-round(tm_length*(1.2/2), 0)}
- else if(tm_length<=100) {lagx<-round(tm_length*(0.9/2), 0)}
- else {lagx<-40}
+  if (tm_length <= 40) {
+    lagx <- round(tm_length * (2/3), 0)
+  } else if (tm_length <= 60) {
+    lagx <- round(tm_length * (1.2/2), 0)
+  } else if (tm_length <= 100) {
+    lagx <- round(tm_length * (0.9/2), 0)
+  } else {
+    lagx <- 30
+  }
 
-if (base::missing(lagxx)) { lagx<-lagx } else { lagx<-lagxx }
+  if (!base::missing(lagx2)) lagx <- lagx2
 
- par(mfrow=c(1,2))
- acff(xdatas, lagx)
- pacff(xdatas, lagx) 
- par(mfrow=c(1,1))
- 
- }
+  ## ---- side-by-side plots ----
+  op <- par(no.readonly = TRUE)
+  par(mfrow = c(1, 2), mar = c(5, 6, 4, 2))
 
+  acff(xdatas, lagx)    # vertical barplot ACF (steelblue)
+  pacff(xdatas, lagx)   # vertical barplot PACF (steelblue)
+
+  par(op)
+}
