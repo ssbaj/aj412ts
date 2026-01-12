3d6be6aad0a8285c1b7666860cc471095912957c
ssbaj <ssb9018@ajou.ac.kr>
2026-01-12 16:37:10 +0900

    first commit

--- R/ccff.R
+++ R/ccff.R
@@ -1,83 +1,48 @@
-ccff <- function(x, y,
-                 lag.max = NULL,
-                 type = c("correlation", "covariance"),
-                 na.action = na.fail,
-                 plot = TRUE, ...) {
+ccff <- function(x, y, lag.max = 10) {
+  # 1. CCF 계산 (데이터 추출용, 그래프는 아직 그리지 않음)
+  # ts.intersect 등을 자동으로 처리해주는 R 내장 ccf 함수 활용
+  out <- ccf(x, y, lag.max = lag.max, plot = FALSE, na.action = na.pass)
   
-  type <- match.arg(type)
+  # 2. 데이터 벡터화 (Lag와 Correlation 값 추출)
+  # as.vector를 사용하여 [n, 1, 1] 형태의 배열을 단순 벡터로 변환
+  lags <- as.vector(out$lag)
+  corrs <- as.vector(out$acf)
   
-  # 입력된 변수명을 문자열로 추출
-  name_x <- deparse(substitute(x))
-  name_y <- deparse(substitute(y))
-  var_names <- paste(name_x, "&", name_y)
+  # 3. 그래프 설정
+  n <- length(x) # 데이터 개수 (유의수준 계산용)
+  main_title <- paste("Cross-Correlation Function\n", 
+                      deparse(substitute(x)), "&", deparse(substitute(y)))
   
-  if (is.matrix(x) || is.matrix(y))
-    stop("univariate time series only")
+  # 4. Barplot 그리기
+  # horiz = TRUE: 막대를 가로로 눕힘 (SAS처럼 Lag가 수직축에 위치)
+  # bp: 각 막대의 중심 y좌표를 저장
+  bp <- barplot(corrs, 
+                horiz = TRUE,          # 가로 막대 (수직 Lag 축)
+                names.arg = lags,      # y축 라벨 (-10, -9 ... 10)
+                col = "steelblue",     # 막대 색상
+                border = NA,           # 테두리 없음
+                xlim = c(-1, 1),       # 상관계수 범위
+                xlab = "Correlation", 
+                ylab = "Lag",
+                main = main_title,
+                las = 1)               # 축 라벨 가로쓰기
   
-  X <- ts.intersect(as.ts(x), as.ts(y))
-  colnames(X) <- c(name_x, name_y)
+  # 테두리 박스
+  box()
   
-  acf.out <- acf(
-    X,
-    lag.max = lag.max,
-    plot = FALSE,
-    type = type,
-    na.action = na.action
-  )
+  # 5. 기준선 추가
+  # (1) 상관계수 0 기준선 (검은 실선)
+  abline(v = 0, col = "black")
   
-  lag <- c(rev(acf.out$lag[-1, 2, 1]),
-           acf.out$lag[, 1, 2])
+  # (2) 유의수준 점선 (빨간 점선, +/- 2/sqrt(n))
+  ci <- 2 / sqrt(n)
+  abline(v = c(-ci, ci), col = "red", lty = 2)
   
-  val <- c(rev(acf.out$acf[-1, 2, 1]),
-           acf.out$acf[, 1, 2])
+  # (3) [요청사항] 각각의 Lag에 해당되는 수평 Dotted Line
+  # bp 변수에 저장된 y좌표를 사용하여 모든 막대 위치에 선을 그립니다.
+  abline(h = bp, lty = 3, col = "gray50")
   
-  n <- length(x)
-  
-  if (plot) {
-    val_rev <- rev(val)
-    lag_rev <- rev(lag)
-    
-    # [수정] 줄바꿈 기호(\n)를 사용하여 변수명을 다음 줄로 이동
-    main_title <- paste0("Number of Data: ", n, "\n", var_names)
-    
-    bp <- barplot(
-      val_rev,
-      horiz  = TRUE,
-      col    = "steelblue",
-      border = NA,
-      xlim   = c(-1, 1),
-      xlab   = "CCF",
-      ylab   = "Lags",
-      names.arg = lag_rev,
-      main = main_title, # 두 줄로 표시되는 타이틀
-      las = 1,
-      cex.main = 0.9     # 타이틀이 두 줄이므로 글자 크기를 약간 조절 (필요시 조정)
-    )
-    
-    box()
-    abline(v = 0)
-    abline(v =  2 / sqrt(n), lty = 3, col = "red")
-    abline(v = -2 / sqrt(n), lty = 3, col = "red")
-    
-    # 5기간 간격 자동 보조선
-    min_lag <- min(lag_rev)
-    max_lag <- max(lag_rev)
-    target_lags <- seq(from = floor(min_lag/5)*5, 
-                       to   = ceiling(max_lag/5)*5, 
-                       by   = 5)
-    
-    for(l in target_lags) {
-      idx <- which(lag_rev == l)
-      if(length(idx) > 0) {
-        line_col <- if(l == 0) "gray20" else "gray70"
-        abline(h = bp[idx], lty = 3, col = line_col) 
-      }
-    }
-  }
-  
-  acf.out$acf <- array(val, dim = c(length(val), 1L, 1L))
-  acf.out$lag <- array(lag, dim = c(length(val), 1L, 1L))
-  acf.out$snames <- paste(acf.out$snames, collapse = " & ")
-  
-  invisible(acf.out)
+  # (선택사항) 막대를 선 위에 다시 그려서 글자나 선이 막대를 가리지 않게 하려면 
+  # 아래 코드를 주석 해제하세요. (지금은 선이 막대 위로 지나가 가독성을 높임)
+  # barplot(corrs, horiz=TRUE, names.arg=lags, col="steelblue", border=NA, add=TRUE, axes=FALSE)
 }
